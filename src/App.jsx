6d93c4a4da740ab51864465b0f7b1c5ac36a42a1
Kasy Jonan <167980745+Kasy256@users.noreply.github.com>
2024-11-04 11:52:07 +0300

    Added content.jsx and content.css files

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import NavMenu from './components/NavMenu';
 import Hero from './components/Hero';
+import  Content from './components/Content';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <NavMenu />
       <Hero />
+      <Content />
     </div>
   );
 }
