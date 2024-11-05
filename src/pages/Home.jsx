d126691718c15c328c0d74added78948a08a04b1
Kasy Jonan <167980745+Kasy256@users.noreply.github.com>
2024-11-05 17:52:46 +0300

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Footer from '../components/Footer';
 
 const Home = () => {
   return (
