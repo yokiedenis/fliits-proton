bc59d6c6e9985a33b3c4626e07ce8bea24ff403e
Kasy Jonan <167980745+Kasy256@users.noreply.github.com>
2024-11-15 16:27:37 +0300

    updates

--- src/pages/AfterSearch.jsx
+++ src/pages/AfterSearch.jsx
@@ -1,13 +1,56 @@
 import React from 'react'
 import BookingHeader from '../components/BookingHeader'
+import { FaFilter } from 'react-icons/fa';
+import '../styles/AfterSearch.css'
 
 function AfterSearch() {
   return (
       <div className='AfterSearch-container'>
           <BookingHeader />
-          <p>hello</p>
-
-
+          <div className="search-Container">
+            <div className="search-filter">
+                <div className="filter-icon">
+                <FaFilter />
+                </div>
+                <div className="filters">
+                    <h5>Price</h5>
+                </div>
+                <div className="filters">
+                    <h5>Vechicle type</h5>
+                </div>
+                <div className="filters">
+                    <h5>Model</h5>
+                </div>
+                <div className="filters">
+                    <h5>Seats</h5>
+                </div>
+                <div className="filters">
+                    <h5>Electric</h5>
+                </div>
+                <div className="filters">
+                    <h5>Deliver</h5>
+                </div>
+                <div className="filters">
+                    <h5>All filters</h5>
+                </div>
+            </div>
+          </div>
+          <div className="separater"></div>
+            <div className="Search-results">
+                <div className="car-results">
+                <h1>Car results</h1>
+                <h1>Car results</h1>
+                <h1>Car results</h1>
+                <h1>Car results</h1>
+                <h1>Car results</h1>
+                </div>
+                <div className="map-results">
+                    <h1>MAPS</h1>
+                    <h1>MAPS</h1>
+                    <h1>MAPS</h1>
+                    <h1>MAPS</h1>
+                </div>
+            </div>
     </div>
   )
 }
